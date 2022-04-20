d4b0ffa9f6c8b0ff807f5eb6b57c1f4bf587d9e3
yanyanshan <yanyanshan@bigo.sg>
2022-04-20 17:24:52 +0800

    feat：路由使用中间件

--- app/router/gameManage/index.js
+++ app/router/gameManage/index.js
@@ -4,19 +4,22 @@
  * @param {Egg.Application} app - egg application
  */
 
-module.exports = ({
-  router,
-  controller: {
-    gameManage: {
-      gameLibrary,
+module.exports = app => {
+  const {
+    middleware,
+    router,
+    controller: {
+      gameManage: {
+        gameLibrary,
+      },
     },
-  },
-}) => {
-
+  } = app
   /** 游戏库*/
+  const responseTime = middleware.responseTime({ headerKey: 'X-Router-Time' }, app)
   router.post('/gameManagement/gameLibrary/add', gameLibrary.addGameSubmit) // 新增游戏
   router.put('/gameManagement/gameLibrary/edit/:id', gameLibrary.editGameSubmit) // 编辑游戏
-  router.get('/gameManagement/gameLibraryList', gameLibrary.getGamesList) // 游戏库页面
+  router.get('/gameManagement/gameLibraryList', responseTime, gameLibrary.getGamesList) // 游戏库页面
+  router.get('/gameMangement/gameLibrary/all', responseTime, gameLibrary.getAllGames) // 游戏库页面
   router.patch('/gameMangement/gameLibrary/updatePutStatus/:id', gameLibrary.updatePutStatus) // 游戏上下架
   router.get('/gameManagement/gameLibraryList/getRelatedTagsBygameId/:gameId', gameLibrary.getRelatedTagsBygameId) // 通过gameId获取关联的标签
 }
